0b6cc55535eee6e38c0f515363cd770adc342440
Viktor Voronin <mobimrzg@gmail.com>
2018-03-24 10:36:16 +0300

    Добавит пакет dotenv, установит переменные окружения

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const morgan = require("morgan");
+require("dotenv");
 
 const app = express();
-
+// configure app
+app.use(morgan("tiny"));
 app.set("view engine", "pug");
 app.set("views", `${__dirname}/templates`);
 
-app.use(morgan("tiny"));
-
 app.get("/", (req, res) => {
-  res.render("index.pug", { title: "Hello" });
+  res.render("index.pug", { title: "Hello", repos: [{ name: "repoo" }] });
 });
 
 app.listen(3000);
