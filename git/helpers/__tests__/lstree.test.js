453d74e6d3d5b7e2575a5d0e2f74b12c833630a8
Viktor Voronin <mobimrzg@gmail.com>
2018-03-27 21:20:54 +0300

    Добавит еще тестов

--- git/helpers/__tests__/lstree.test.js
+++ git/helpers/__tests__/lstree.test.js
@@ -10,6 +10,8 @@ const getLsTreeTextLines = () => {
 
 const findFilesByDir = (files, dir) => files.filter(f => f.getDir() === dir);
 
+const findFileByName = (files, name) => files.find(f => f.getName() === name);
+
 describe("Тестирует parseGitLstreeOutput()", () => {
   it("Корневой каталог содержит правильный список файлов", () => {
     const lines = getLsTreeTextLines();
@@ -18,5 +20,31 @@ describe("Тестирует parseGitLstreeOutput()", () => {
     const expectedFileList = [".gitignore", "anotherfile.txt", "dir1", "dir2", "somefile.txt"];
     const allFilesPresent = rootDirFiles.every(f => expectedFileList.includes(f.getName()));
     expect(allFilesPresent).toBe(true);
+  });
+
+  it("Вложенная директория содержит правильный список файлов", () => {
+    const lines = getLsTreeTextLines();
+    const files = parseGitLstreeOutput(lines);
+    const dirFiles = findFilesByDir(files, "dir1");
+    const expectedFileList = ["dir1_file.txt", "innerdir"];
+    const allFilesPresent = dirFiles.every(f => expectedFileList.includes(f.getName()));
+    expect(allFilesPresent).toBe(true);
+  });
+
+  it("У корневого файла правильный родитель", () => {
+    const lines = getLsTreeTextLines();
+    const files = parseGitLstreeOutput(lines);
+    const childFile = findFileByName(files, ".gitignore");
+    const expectedParent = null;
+    expect(childFile.getParent()).toEqual(expectedParent);
+  });
+
+  it("У вложенного файла правильный родитель", () => {
+    const lines = getLsTreeTextLines();
+    const files = parseGitLstreeOutput(lines);
+    const childFile = findFileByName(files, "inner.txt");
+    const actualParent = childFile.getParent();
+    const expectedParent = findFileByName(files, "innerdir");
+    expect(actualParent).toEqual(expectedParent);
   });
 });
